d8ff66f18787b59e630df88cab2f9e432c5ef591
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-01-15 18:29:57 -0500

    feat ✨: Crea el modelo de Producto

---
+++ Models/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AP1GestionInventario.Models;
+
+public class Producto
+{
+    [Key]
+    public int ProductoId { get; set; }
+
+    [Required(ErrorMessage = "La descripción es obligatoria")]
+    [StringLength(200)]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El costo es obligatorio")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El costo debe ser mayor a 0")]
+    public decimal Costo { get; set; }
+
+    [Required(ErrorMessage = "El precio es obligatorio")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+    public decimal Precio { get; set; }
+
+    public int Existencia { get; set; }
+}
